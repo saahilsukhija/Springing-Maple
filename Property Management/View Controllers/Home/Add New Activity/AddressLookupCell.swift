30420ffe3cc115b453b045dc35e0e61609a4749f
Saahil Sukhija <saahilsukhija@gmail.com>
2024-04-05 00:34:42 -0700

    Added address lookups

---
+++ Property Management/View Controllers/Home/Add New Activity/AddressLookupCell.swift
@@ -0,0 +1,23 @@
+//
+//  AddressLookupCell.swift
+//  Property Management
+//
+//  Created by Saahil Sukhija on 4/5/24.
+//
+
+import UIKit
+
+class AddressLookupCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
