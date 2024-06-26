5ab293c7221740212431c7ae9385a9ce3c349855
Saahil Sukhija <saahilsukhija@gmail.com>
2024-06-26 12:55:40 -0700

    Added PhotoUpload

---
+++ Property Management/Cloud Helpers/Class Extensions/ImageCompressor.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCompressor.swift
+//  Property Management
+//
+//  Created by Saahil Sukhija on 6/26/24.
+//
+
+import Foundation
---
+++ Property Management/View Controllers/Photo Upload/PhotoUploadVC.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoUploadVC.swift
+//  Property Management
+//
+//  Created by Saahil Sukhija on 6/24/24.
+//
+
+import UIKit
+
+class PhotoUploadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
